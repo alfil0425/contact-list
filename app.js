9a454312f258f441a0b0e16a94c65c231155ca95
mauricio <mauricio.leyva2020@gmail.com>
2023-06-06 08:53:57 -0500

    se añade eliminar

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ let listadecontactos =[
 function añadircontacto (nuevo){
     listadecontactos.push(nuevo);
 }
+
 añadircontacto("Isaac Leyva");
 
+
+function eliminar (){
+    listadecontactos.shift();
+}
+
+eliminar();
+
 console.log(listadecontactos);
+
+
+
+
+
