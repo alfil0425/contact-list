a18d54f6546939fabd284ca0f22c7d227bafb277
mauricio <mauricio.leyva2020@gmail.com>
2023-06-06 07:34:42 -0500

    creacion lista de contactos

---
+++ app.js
@@ -0,0 +1,12 @@
+let listadecontactos =[
+    "Luis Cubillos",
+    "Mauricio Leyva",
+    "Grace Rodriguez"
+    
+]
+function añadircontacto (nuevo){
+    listadecontactos.push(nuevo);
+}
+añadircontacto("Isaac Leyva");
+
+console.log(listadecontactos);
